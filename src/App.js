6286ebbbda7ba0b204af1229cc48f9cbdc013380
Eric Avila <avilaeric295@gmail.com>
2022-04-08 18:49:04 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Slider from './components/slider';
+
+function App() {
+  return (
+    <div className="App">
+      <Slider></Slider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/exports/images.js
@@ -0,0 +1,18 @@
+import arboles from '../assets/arboles.jpg'
+import cielo from '../assets/cielo.jpg'
+import faro from '../assets/faro.jpg'
+import inspire from '../assets/inspire.jpg'
+import luna from '../assets/luna.jpg'
+import mars from '../assets/mars.jpg'
+import marte from '../assets/marte.jpg'
+import ojos from '../assets/ojos.jpg'
+import quimica from '../assets/quimica.jpg'
+import rio from '../assets/rio.jpg'
+import rocas from '../assets/rocas.jpg'
+import touch from '../assets/touch.jpg'
+import edificio from '../assets/edificio.jpg'
+import people from '../assets/people.jpg'
+
+
+
+export default [arboles, cielo, faro, inspire, luna, mars, marte, ojos, quimica, rio, rocas, touch, edificio, people]
